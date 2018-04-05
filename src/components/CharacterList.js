981cb61ff69c1b4ab0dc7e11fae6c174ba980bf5
Septian A Tama <sfirst.tama@gmail.com>
2018-04-05 11:08:08 +0700

    mapping state and dispatch to component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { loadCharas } from '../store/characters/characters.actions';
 
 class CharacterList extends Component {
+
+  componentDidMount () {
+    this.props.loadCharas()
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Nanti chara list di sini</h1> 
-      </div>
-    );
+    if (this.props.charas.loading) {
+      return <h1>Lagi Loading . . . </h1>;
+    } else if (this.props.charas.error) {
+      return <h1>Oops, ada error!</h1>;
+    } else {
+      return (
+        <div>
+          <h1>Nanti chara list di sini</h1>
+          <ul>
+            {
+              this.props.charas.data.map(chara => (
+                <li key={chara.url}>{ chara.name }</li>
+              ))
+            }
+          </ul>
+        </div>
+      )
+    }
   }
 }
 
-export default CharacterList;
+const mapStateToProps = (state) => ({
+  charas: state.charas,
+});
+
+//https://redux.js.org/api-reference/bindactioncreators
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  loadCharas,
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
